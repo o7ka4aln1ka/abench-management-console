08444ae368a4518fde2949575b390bd927852e32
Vasil Radushev <r_vasil@yahoo.com>
2019-05-25 15:48:15 +0200

    minor changes

--- scripts/activate_scripts.sh
+++ scripts/activate_scripts.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-(gnome-terminal -- find ~/github/a-bench-dashboard/scripts -type f -iname "*.sh" -exec chmod +x {} &&\
-echo All .sh scripts were made executable\ &&
-gnome-terminal -- find ~/github/a-bench-dashboard/scripts -type f -iname "*.py" -exec chmod +x {} &&\
-echo All .py scripts were made executable\
+(cd ~/github/a-bench-dashboard/scripts -type f -iname "*.sh" -exec chmod +x {}
+echo All .sh scripts were made executable
+cd ~/github/a-bench-dashboard/scripts -type f -iname "*.py" -exec chmod +x {}
+echo All .py scripts were made executable
+pwd
 ) 2>&1 | tee -a ~/github/a-bench-dashboard/output-homepage.txt
